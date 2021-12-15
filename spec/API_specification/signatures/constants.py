103a8fbd437170a579cedaa844c30db6e1c38ce8
Stephannie Jimenez <steff456@hotmail.com>
2021-12-15 12:22:18 -0500

    Transform constants.md to rst format

---
+++ spec/API_specification/signatures/constants.py
@@ -0,0 +1,25 @@
+e = None
+"""
+IEEE 754 floating-point representation of Euler's constant.
+
+``e = 2.71828182845904523536028747135266249775724709369995...``
+"""
+
+inf = None
+"""
+IEEE 754 floating-point representation of (positive) infinity.
+"""
+
+nan = None
+"""
+IEEE 754 floating-point representation of Not a Number (``NaN``).
+"""
+
+pi = None
+"""
+IEEE 754 floating-point representation of the mathematical constant ``π``.
+
+``pi = 3.1415926535897932384626433...``
+"""
+
+__all__ = ['e', 'inf', 'nan', 'pi']
