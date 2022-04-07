2b9484bcc13f7c67d9105a6997ddddd33388c30c
Athan Reines <kgryte@gmail.com>
2022-04-07 02:47:56 -0700

    Update copy

--- spec/API_specification/signatures/indexing_functions.py
+++ spec/API_specification/signatures/indexing_functions.py
@@ -12,7 +12,7 @@ def take(x: array, indices: Union[int, array], /, *, axis: int) -> array:
     x: array
         input array.
     indices: Union[int, array]
-        array indices. If ``indices`` is an array, the array must be one-dimensional and have an integer data type. If ``indices`` is an integer, the function must follow single-axis indexing semantics (see :ref:`indexing`).
+        array indices. If ``indices`` is an array, the array must be one-dimensional and have an integer data type. If ``indices`` is an integer, the function must follow multi-axis indexing semantics when providing an integer for a single-axis index (see :ref:`indexing`).
     axis: int
         axis over which to select values. If ``axis`` is negative, the function must determine the axis along which to select values by counting from the last dimension.
 
