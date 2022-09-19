0024b6a5076d35514c1775dcd61126e2beee955c
Athan Reines <kgryte@gmail.com>
2022-09-19 01:32:53 -0700

    Fix missing import

--- spec/API_specification/array_api/__init__.py
+++ spec/API_specification/array_api/__init__.py
@@ -4,6 +4,7 @@ from .creation_functions import *
 from .data_type_functions import *
 import array_api.data_types as dtype
 from .elementwise_functions import *
+from .indexing_functions import *
 from .linear_algebra_functions import *
 from .manipulation_functions import *
 from .searching_functions import *
