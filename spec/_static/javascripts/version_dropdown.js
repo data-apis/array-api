5c124b81711ac51ff45963b5c698308e48d8f475
Athan Reines <kgryte@gmail.com>
2023-07-08 16:03:34 -0700

    Refactor to use async/await and update URL manipulation logic

--- spec/_static/javascripts/version_dropdown.js
+++ spec/_static/javascripts/version_dropdown.js
@@ -1,46 +1,114 @@
-function assign_href(a, url, path) {
-    fetch(url + "/" + path).then(response => {
+/**
+* Returns a URL corresponding to a versioned resource (if one exists).
+*
+* @private
+* @param {string} url - base URL
+* @param {string} path - resource path
+* @returns {Promise} promise which resolves a resource URL
+*/
+async function href(url, path) {
+    const defaultURL = url + "/index.html";
+    url += "/" + path;
+    await fetch(url).then(onResponse).catch(onError);
+
+    /**
+    * Success handler.
+    *
+    * @private
+    * @param {Object} response - response object
+    */
+    function onResponse(response) {
         if (response.ok) {
-            a.href = url + "/" + path;
-        } else {
-            a.href = url + "/index.html";
+            return url;
         }
-    }).catch(error => {
-        a.href = url + "/index.html";
-    });
+        return defaultURL;
+    }
+
+    /**
+    * Error handler.
+    *
+    * @private
+    * @param {Error} error - error object
+    */
+    function onError(error) {
+        return defaultURL;
+    }
 }
 
-function add_version_dropdown(json_loc, target_loc, text) {
-    var dropdown = document.createElement("div");
+/**
+* Adds a version dropdown menu with custom URL paths depending on the current page.
+*
+* @param {string} json_loc - JSON URL
+* @param {string} target_loc - target URL
+* @param {string} text - text
+* @returns {Promise} promise which resolves upon adding a version menu
+*/
+async function add_version_dropdown(json_loc, target_loc, text) {
+    const dropdown = document.createElement("div");
     dropdown.className = "md-flex__cell md-flex__cell--shrink dropdown";
-    var button = document.createElement("button");
+
+    const button = document.createElement("button");
     button.className = "dropdownbutton";
-    var content = document.createElement("div");
+
+    const content = document.createElement("div");
     content.className = "dropdown-content md-hero";
+
     dropdown.appendChild(button);
     dropdown.appendChild(content);
-    $.getJSON(json_loc, function(versions) {
-        var currentURL = window.location.href;
-        var path = currentURL.split(/_site|array_api/)[1];
+
+    await $.getJSON(json_loc, onVersions).done(onDone).fail(onFail).always(onAlways);
+
+    /**
+    * Callback invoked upon successfully resolving a list of versions.
+    *
+    * @private
+    * @param {Object} versions - versions object
+    */
+    async function onVersions(versions) {
+        const currentURL = window.location.href;
+        let path = currentURL.split(/_site|array_api/)[1];
         if (path) {
             path = path.split("/");
             path = path.slice(2, path.length);
             path = path.join("/");
-            for (var key in versions) {
-                if (versions.hasOwnProperty(key)) {
-                    var a = document.createElement("a");
-                    a.innerHTML = key;
-                    a.title = key;
-                    assign_href(a, target_loc + versions[key], path);
-                    content.appendChild(a);
-                }
+        } else {
+            path = "";
+        }
+        for (let key in versions) {
+            if (versions.hasOwnProperty(key)) {
+                let a = document.createElement("a");
+                a.innerHTML = key;
+                a.title = key;
+                a.href = await href(target_loc + versions[key], path);
+                content.appendChild(a);
             }
         }
-    }).done(function() {
+    }
+
+    /**
+    * Callback invoked upon resolving a JSON resource.
+    *
+    * @private
+    */
+    function onDone() {
         button.innerHTML = text;
-    }).fail(function() {
+    }
+
+    /**
+    * Callback invoked upon failing to resolve a JSON resource.
+    *
+    * @private
+    */
+    function onFail() {
         button.innerHTML = "Other Versions Not Found";
-    }).always(function() {
+    }
+
+    /**
+    * Callback which is always invoked upon attempting to resolve a JSON resource.
+    *
+    * @private
+    */
+    function onAlways() {
         $(".navheader").append(dropdown);
-    });
+    }
 };
