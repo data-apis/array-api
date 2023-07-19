f3156745713d7dd4e3250d4d3b41d8b8583c3e41
Stephannie Jimenez <steff456@hotmail.com>
2023-07-19 16:57:16 -0500

    Add version to the nav title

--- spec/2021.12/conf.py
+++ spec/2021.12/conf.py
@@ -6,4 +6,8 @@ from array_api_stubs import _2021_12 as stubs_mod
 from _array_api_conf import *
 
 release = "2021.12"
+
+nav_title = html_theme_options.get("nav_title") + " v{}".format(release)
+html_theme_options.update({"nav_title": nav_title})
+
 sys.modules["array_api"] = stubs_mod
--- spec/2022.12/conf.py
+++ spec/2022.12/conf.py
@@ -6,4 +6,7 @@ from array_api_stubs import _2022_12 as stubs_mod
 from _array_api_conf import *
 
 release = "2022.12"
+
+nav_title = html_theme_options.get("nav_title") + " v{}".format(release)
+html_theme_options.update({"nav_title": nav_title})
 sys.modules["array_api"] = stubs_mod
