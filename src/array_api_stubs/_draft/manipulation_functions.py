b93274f73f56d6813f3b5fcc647f0c1aa49e19f6
Athan Reines <kgryte@gmail.com>
2024-01-25 04:17:19 -0800

    Add admonition regarding data-dependent output shape

--- src/array_api_stubs/_draft/manipulation_functions.py
+++ src/array_api_stubs/_draft/manipulation_functions.py
@@ -170,6 +170,11 @@ def repeat(
     """
     Repeats elements of an array.
 
+    .. admonition:: Data-dependent output shape
+        :class: important
+
+        The shape of the output array for this function depends on the data values in the ``repeats`` array; hence, array libraries which build computation graphs (e.g., JAX, Dask, etc.) may find this function difficult to implement without knowing array values. Accordingly, such libraries may choose to omit this function. See :ref:`data-dependent-output-shapes` section for more details.
+
     Parameters
     ----------
     x: array
