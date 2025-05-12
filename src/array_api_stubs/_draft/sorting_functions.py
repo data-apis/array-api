c449b558ffca210f6fd48c977ad58fba960a23ee
Athan <kgryte@gmail.com>
2025-05-11 22:45:52 -0700

    docs: fix markup

--- src/array_api_stubs/_draft/sorting_functions.py
+++ src/array_api_stubs/_draft/sorting_functions.py
@@ -13,23 +13,23 @@ def argsort(
     Parameters
     ----------
     x: array
-        input array. *Should* have a real-valued data type.
+        input array. **Should** have a real-valued data type.
     axis: int
-        axis along which to sort. If set to ``-1``, the function *must* sort along the last axis. Default: ``-1``.
+        axis along which to sort. If set to ``-1``, the function **must** sort along the last axis. Default: ``-1``.
     descending: bool
-        sort order. If ``True``, the returned indices *must* sort ``x`` in descending order (by value). If ``False``, the returned indices *must* sort ``x`` in ascending order (by value). Default: ``False``.
+        sort order. If ``True``, the returned indices **must** sort ``x`` in descending order (by value). If ``False``, the returned indices **must** sort ``x`` in ascending order (by value). Default: ``False``.
     stable: bool
-        sort stability. If ``True``, the returned indices *must* maintain the relative order of ``x`` values which compare as equal. If ``False``, the returned indices *may* maintain the relative order of ``x`` values which compare as equal (i.e., the relative order of ``x`` values which compare as equal is implementation-dependent). Default: ``True``.
+        sort stability. If ``True``, the returned indices **must** maintain the relative order of ``x`` values which compare as equal. If ``False``, the returned indices **may** maintain the relative order of ``x`` values which compare as equal (i.e., the relative order of ``x`` values which compare as equal is implementation-dependent). Default: ``True``.
 
     Returns
     -------
     out: array
-        an array of indices. The returned array *must* have the same shape as ``x``. The returned array *must* have the default array index data type.
+        an array of indices. The returned array **must** have the same shape as ``x``. The returned array **must** have the default array index data type.
 
     Notes
     -----
 
-    -  For backward compatibility, conforming implementations *may* support complex numbers; however, inequality comparison of complex numbers is unspecified and thus implementation-dependent (see :ref:`complex-number-ordering`).
+    -  For backward compatibility, conforming implementations **may** support complex numbers; however, inequality comparison of complex numbers is unspecified and thus implementation-dependent (see :ref:`complex-number-ordering`).
     """
 
 
@@ -42,21 +42,21 @@ def sort(
     Parameters
     ----------
     x: array
-        input array. *Should* have a real-valued data type.
+        input array. **Should** have a real-valued data type.
     axis: int
-        axis along which to sort. If set to ``-1``, the function *must* sort along the last axis. Default: ``-1``.
+        axis along which to sort. If set to ``-1``, the function **must** sort along the last axis. Default: ``-1``.
     descending: bool
-        sort order. If ``True``, the array *must* be sorted in descending order (by value). If ``False``, the array *must* be sorted in ascending order (by value). Default: ``False``.
+        sort order. If ``True``, the array **must** be sorted in descending order (by value). If ``False``, the array **must** be sorted in ascending order (by value). Default: ``False``.
     stable: bool
-        sort stability. If ``True``, the returned array *must* maintain the relative order of ``x`` values which compare as equal. If ``False``, the returned array *may* maintain the relative order of ``x`` values which compare as equal (i.e., the relative order of ``x`` values which compare as equal is implementation-dependent). Default: ``True``.
+        sort stability. If ``True``, the returned array **must** maintain the relative order of ``x`` values which compare as equal. If ``False``, the returned array **may** maintain the relative order of ``x`` values which compare as equal (i.e., the relative order of ``x`` values which compare as equal is implementation-dependent). Default: ``True``.
 
     Returns
     -------
     out: array
-        a sorted array. The returned array *must* have the same data type and shape as ``x``.
+        a sorted array. The returned array **must** have the same data type and shape as ``x``.
 
     Notes
     -----
 
-    -   For backward compatibility, conforming implementations *may* support complex numbers; however, inequality comparison of complex numbers is unspecified and thus implementation-dependent (see :ref:`complex-number-ordering`).
+    -   For backward compatibility, conforming implementations **may** support complex numbers; however, inequality comparison of complex numbers is unspecified and thus implementation-dependent (see :ref:`complex-number-ordering`).
     """
